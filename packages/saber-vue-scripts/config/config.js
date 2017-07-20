eea896d557ea369f5838264ed74d7947e348f49c
郭超 <guochao@puhuifinance.com>
2017-07-20 16:13:34 +0800

    初步完成项目基本命令

---
+++ packages/saber-vue-scripts/config/config.js
@@ -0,0 +1,29 @@
+const paths = require('./paths')
+const _ = require('lodash')
+const fs = require('fs')
+
+function getBlueConfig() {
+    return fs.existsSync(paths.appConfig) ? require(paths.appConfig) : {}
+}
+
+let saberConfigDefaults = {
+    build: {
+        env {
+            NODE_ENV: '"production"'
+        },
+        assetsSubDirectory: 'static',
+        assetsPublicPath: '/',
+    }
+    dev: {
+        env: {
+            NODE_ENV: '"development"'
+        },
+        port: 8080,
+        proxyTable: {},
+        assetsSubDirectory: 'static',
+        assetsPublicPath: '/',
+        cssSourceMap: false
+    }
+}
+
+module.exports = _.merge({}, saberConfigDefaults, getBlueConfig())
--- packages/saber-vue-scripts/config/paths.js
+++ packages/saber-vue-scripts/config/paths.js
@@ -5,3 +5,16 @@ const fs = require('fs');
 const url = require('url');
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+module.exports = {
+    appDirectory,
+    appDist: resolveApp('dist'),
+    appHTML: resolveApp('index.html'),
+    appIndexJs: resolveApp('src/main.js'),
+    appSrc: resolveApp('src'),
+    appConfig: resolveApp('saber.config.js'),
+    appNodeModules: resolveApp('node_modules'),
+    appAssents: resolveApp('src/assets'),
+    appStatic: resolveApp('static'),
+    appAssetsPublicPath: appDirectory,
+}
